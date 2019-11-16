19c134db69da74a761a646a96239bf0dcff82c41
Юленька <julia_s_94@mail.ru>
2019-11-16 16:45:57 +0300

    refactoring

--- src/main/java/core/DeskRunner.java
+++ src/main/java/core/DeskRunner.java
@@ -5,8 +5,10 @@ public class DeskRunner {       //точка входа в программу
         Desk studentDesk = new Desk ("wood", 1.75, 0.5, 0.7);
         System.out.println(studentDesk.getColor());
         System.out.println(studentDesk.toString());
-        studentDesk.color = "red";
+//        studentDesk.color = "red";
         System.out.println(studentDesk.toString());
+        studentDesk.hashCode();
+
     }
 
 
